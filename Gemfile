4c9a572635479661fa4cb2baddd30aca4f7a335d
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-30 15:51:21 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.1'
+
+gem 'rails-api'
+gem 'thin'
+gem 'active_model_serializers', :github => 'rails-api/active_model_serializers'
+
+group :development, :test do
+  gem 'debugger'
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'compass-rails'
+  gem 'uglifier'
+  gem 'bootstrap-sass'
+  gem 'handlebars_assets'
+end
+
+group :development do
+  gem 'quiet_assets'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+EmberApp::Application.config.secret_token = '3647bfa5c1fd4b68ef6d2db83cd906307167626ec93f26a0d4449eebdc20b701e98b4622a3e1680a9931e5397813de3413bad8c4abd85df017fe0210ae9d0924'
